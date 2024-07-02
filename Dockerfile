fd495482291aae1c56bd3f2a7317285ace6d3ab3
Pablo Prats <pablo.prats@um.edu.ar>
2024-07-02 18:08:10 -0300

    add logging - fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM python:3.10-slim-bullseye
 
-ENV GECKODRIVER_VER=v0.31.0
-ENV FLASK_ENV=production
+ENV FLASK_CONTEXT=production
 ENV PYTHONUNBUFFERED=1
 ENV PATH=$PATH:/home/flaskapp/.local/bin
 
 RUN useradd --create-home --home-dir /home/flaskapp flaskapp
+RUN apt-get update
+RUN apt-get install -y python3-dev build-essential libpq-dev python3-psycopg2
+RUN apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false
+RUN rm -rf /var/lib/apt/lists/*
 
 WORKDIR /home/flaskapp
 
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from app import create_app
-
+import logging
+# Ref: https://docs.python.org/3/library/logging.html
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 app = create_app()
 
 #https://flask.palletsprojects.com/en/3.0.x/appcontext/
@@ -11,5 +13,5 @@ if __name__ == '__main__':
     Ref: https://flask.palletsprojects.com/en/3.0.x/api/#flask.Flask.run
     Ref: Book Flask Web Development Page 9
     """
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", debug=False, port=5000)
     
